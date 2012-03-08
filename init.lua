1119747126962a6ef0a2eee0341f4097814b1283
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2012-03-09 02:19:05 +0400

    minor modifications and comments to draft lua code

--- init.lua
+++ init.lua
@@ -6,13 +6,18 @@
 
 
 -- set apkg_result to the required return code
-apkg_result=1
+apkg_result=0
 
 common_help = "(c) AgiliaLinux Developers 2012, Some rights reserved";
 
 
 applets={};
 
+-- The code below demonstrates a simple mechanism to register new applets in the 
+-- app code. 
+-- TODO: ByteCompilation of applets in different files
+-- TODO: Freaking localisation of messages support. 
+-- TODO: everything else
 function register_applet(applet)
 	print("Registering applet "..applet['name']);
 	applets[applet['name']]=applet;
@@ -22,11 +27,15 @@ applet_apkg = {
 	name = 'apkg';
 	help = "This utility should not be called directly, use apkg-* instead",
 	minargs = 0;
-	applet = function()
-	print(applet_apkg['help'])
-end
+	applet = function(args)
+		-- apkg does nothing by default
+		print("APKG :: AgiliaLinux Package Manager");
+		print(applet_apkg['help'])
+		-- and returns a bad result code, since it shouldn't be called
+		apkg_result=2;
+	end
 }
-register_applet(applet_apkg);
 
-applets[apkg_applet]
-print(applets[apkg_applet]['help']);
+-- TODO: send the rest of argv to lua
+register_applet(applet_apkg);
+applets[apkg_applet]['applet'](nil);
